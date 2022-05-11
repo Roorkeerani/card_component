e388c61849372fbca9ae81718e05aedd815f0811
Roorkee <antakshyarisahoo99@gmail.com>
2022-05-11 13:27:06 +0530

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { pricingTable } from '../pricingTable/pricingTable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    pricingTable
   ],
   imports: [
     BrowserModule
@@ -14,3 +16,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/pricingTable/pricingTable.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({ 
+    selector:'app-pricingTable',
+    templateUrl:'./pricingTable.component.html',
+    styleUrls:['./pricingTable.component.css']
+})
+export class pricingTable{
+    
+}
